33eea0b6c1662af950adc4e1fc68f1515563c083
TyomPapiyan <papiyan.artyom@gmail.com>
2026-06-04 23:37:50 +0400

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,1 @@
-from .db import init_db, upsert_user, save_entry, update_entry_analysis, get_entries, get_entries_by_date, get_entry_count
+from .db import init_db, upsert_user, save_entry, update_entry_analysis, get_entries, get_entries_by_date, get_entry_count, set_user_language, get_user_language
